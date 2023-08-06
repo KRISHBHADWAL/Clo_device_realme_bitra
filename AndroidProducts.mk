006400d56675bb14fd7a436e723deec4fd63f81a
Krish <drkrish.20@gmail.com>
2023-08-06 17:19:46 +0530

    Bitra: Project404 BringUp!

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2023 Project 404
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/p404_davinci.mk
+
+COMMON_LUNCH_CHOICES := \
+    p404_bitra-user \
+    p404_bitra-userdebug \
+    p404_bitra-eng
